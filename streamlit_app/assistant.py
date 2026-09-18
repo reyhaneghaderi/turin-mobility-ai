367ec4a1dedca17d08158f2806feb9de1c74539b
Reyhane Ghaderi <reyhanehghaderi63@gmail.com>
2026-09-18 18:06:16 +0200

    Update Neon database connection

--- streamlit_app/assistant.py
+++ streamlit_app/assistant.py
@@ -19,9 +19,14 @@ client = OpenAI(
 )
 
 
-database_url = st.secrets["DATABASE_URL"]
-
-conn = psycopg2.connect(database_url)
+conn = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    sslmode="require"
+)
 
 
 base_dir = os.path.dirname(
